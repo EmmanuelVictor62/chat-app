856c9846c776f307b6cdbf85c76e70cbfef8edc7
royalManuel <104584134+EmmanuelVictor62@users.noreply.github.com>
2025-02-10 20:32:36 +0100

    feat: Implemented swagger docs

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,5 +3,6 @@ import app from "./app";
 const PORT = 5334;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
